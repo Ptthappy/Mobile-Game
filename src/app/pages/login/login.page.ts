a15fbe2536fe3eedbfff2502d1933a94618d7021
Luis Petrella <aax_00ap@hotmail.com>
2019-07-20 12:53:52 -0400

    Login and register pages half-finished

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigateService } from '../../services/navigate.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  user: string = '';
+  pass: string = '';
+  fetched: boolean = false;
+
+  constructor(private navigator: NavigateService) { }
+
+  login = async () => {
+    if (this.user === '' || this.pass === '') {
+      console.log("Please enter all the fields");  // TODO: Show error label
+    } else if (!this.fetched) {
+      const body = {
+        username: this.user,
+        password: this.pass
+      };
+
+      console.log(body);
+
+      this.fetched = true;
+      await fetch('http://localhost:8080/login', { body: JSON.stringify(body), method: 'POST', credentials: 'include' })
+          .then(res => res.json())
+          .then(res => {
+            if (res.status === 200) {   // TODO: use local storage to save user
+              console.log('Te logiaste jaja');
+              this.navigator.navigate('menu');
+            } else {
+              console.log('No te logiaste jaja'); // TODO: Show error label with incorrect user or password
+            }
+          }).catch(err => {
+            console.log('Error trying to login. Check your Internet connectuion and try again'); // TODO: Move this to error label
+            this.fetched = false;
+          });
+    }
+  }
 
   ngOnInit() {
   }
--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigateService } from '../../services/navigate.service';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  user: string = '';
+  email: string = '';
+  pass: string = '';
+  pass2: string = '';
+  fetched: boolean = false;
+
+  constructor(private navigator: NavigateService) { }
+
+  register = async () => {
+    if (this.user === '' || this.email === '' || this.pass === '' || this.pass2 === '') {
+      console.log("Please enter all the fields");
+    } else if (this.pass !== this.pass2) {
+      console.log('Passwords doesn\'t match');
+    } else if (!this.fetched) {
+      const body = {
+        username: this.user,
+        email: this.email,
+        password: this.pass
+      };
+
+      this.fetched = true;
+
+      await fetch('http://localhost:8080/register', { method: 'POST', body: JSON.stringify(body), credentials: 'include' })
+          .then(res => res.json())
+          .then(res => {
+            if (res.status === 200) {
+              console.log('te rejistratez mirrei jajjaajaj');
+            } else {
+              console.log('no te rejistrastez plastemierda');
+            }
+          }).catch(err => {
+            console.log('mamawebo si no hay server como sebillo te vai a rejistra XD');
+          });
+      this.fetched = false;
+    }
+  }
 
   ngOnInit() {
   }
